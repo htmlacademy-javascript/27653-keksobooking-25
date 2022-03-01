071a16e5b509baa01b126ae4a5b256583a7d2c8a
Olga <ovk2863@gmail.com>
2022-03-01 12:19:59 +0300

    Изменяет функции на стрелочные

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-function getRandomPositiveNumber(min, max) {
-  if(min < 0 || min > max) {
-    return 0;
-  }
-  return min + Math.floor(Math.random() * (max + 1 - min));
-}
-getRandomPositiveNumber(0, 20);
+const getRandomPositiveNumber = (min, max) => {
+  min = Math.ceil(Math.abs(min));
+  max = Math.floor(Math.abs(max));
 
-function getRandomFloatNumber(min, max, digits) {
-  if(min < 0 || min > max) {
-    return 0;
+  if(min > max){
+    return max + Math.floor(Math.random() * (min + 1 - max));
   }
+
+  return min + Math.floor(Math.random() * (max + 1 - min));
+};
+
+getRandomPositiveNumber();
+
+const getRandomFloatNumber = (min, max, digits) => {
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  if(min > max) {
+    return Number((max + Math.random() * (min - max)).toFixed(digits));
+  }
+
   return Number((min + Math.random() * (max - min)).toFixed(digits));
-}
-getRandomFloatNumber(1.1, 1.2, 3);
+};
+
+getRandomFloatNumber();
