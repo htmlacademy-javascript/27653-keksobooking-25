4f857f954888326d0cc860fbb0e8ef3575873fb4
Olga <ovk2863@gmail.com>
2022-03-13 18:25:37 +0300

    Генерирует разметку на основе данных

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-import {createAd, COUNT_ADS} from './data.js';
-
-const similarAds = Array.from({length: COUNT_ADS}).map((item, idx) => createAd(idx));
-
-console.log(similarAds);
+import './card.js';
